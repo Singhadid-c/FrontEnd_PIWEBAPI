9466faf2098024eb5634cd10b44b282e14fed158
Singhadid-C <singhadid.c@hotmail.com>
2020-04-24 16:50:13 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { MenuComponent } from './components/layout/menu/menu.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { DashboardComponent } from './components/content/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
